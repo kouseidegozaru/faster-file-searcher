579ef35b93f22741050df5e4f4df279b48d2c627
kouseidegozaru <gguangchui@gmail.com>
2024-09-24 12:13:21 +0900

    条件の評価を高速化 4.0s

--- fileFinder.py
+++ fileFinder.py
@@ -1,24 +1,23 @@
 import os
 
-def is_match(file_name, *args):
-    return all(map(lambda arg: arg in file_name, args))
+def is_match(file_name, filter_options):
+    # filter_optionsをセットに変換
+    filter_set = set(filter_options)
+    # 全てのフィルター条件を満たすか確認
+    return filter_set.issubset(file_name)
 
-def file_search(target_dir_path, *args):
-
-    filter_options = args
-
+def file_search(target_dir_path, *filter_options):
     try:
-        # os.scandirを使い、ファイルやディレクトリの情報をジェネレーターで得る
+        #ディレクトリ下の情報を取得する
         with os.scandir(target_dir_path) as entries:
             for entry in entries:
-                
+
                 # フィルターを適用する
-                if is_match(entry.name, *filter_options):
+                if is_match(entry.name, filter_options):
                     yield entry.path  # ファイルパスを返す
-                
+
                 # サブディレクトリを探索する
                 if entry.is_dir():
-                    # サブディレクトリに対して再帰処理を行う
                     yield from file_search(entry.path, *filter_options)
 
     except PermissionError:
