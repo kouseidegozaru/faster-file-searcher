b57b2bcddb241d12b08b64176d509d3c2a4ecbb1
kouseidegozaru <gguangchui@gmail.com>
2024-09-24 17:24:15 +0900

    処理時間の計測

--- fileFinder.py
+++ fileFinder.py
@@ -1,4 +1,5 @@
 import os
+from time_counter import timer
 
 def is_match(file_name, filter_options):
     # filter_optionsをセットに変換
@@ -7,18 +8,46 @@ def is_match(file_name, filter_options):
     return filter_set.issubset(file_name)
 
 def file_search(target_dir_path, *filter_options):
-    try:
-        #ディレクトリ下の情報を取得する
-        with os.scandir(target_dir_path) as entries:
-            for entry in entries:
 
-                # フィルターを適用する
+    match_time = timer()
+    scan_time = timer()
+
+    # ディレクトリ内のエントリーを非同期でスキャンするジェネレーター関数
+    def scan_dir(target_dir_path):
+        try:
+            # ディレクトリのスキャンをタイミング計測
+            @scan_time.timecount
+            def scan():
+                return os.scandir(target_dir_path)
+
+            # ディレクトリ内のエントリーをジェネレーターとして返す
+            for entry in scan():
+                yield entry
+
+        except PermissionError:
+            # アクセス許可がない場合はスキップ
+            pass
+
+    # 内部探索関数
+    def inner(target_dir_path, *filter_options):
+        for entry in scan_dir(target_dir_path):
+
+            # フィルタに一致するかの判定をタイミング計測
+            @match_time.timecount
+            def match():
                 if is_match(entry.name, filter_options):
-                    yield entry.path  # ファイルパスを返す
+                    yield entry.path  # 一致したファイルパスを返す
 
-                # サブディレクトリを探索する
-                if entry.is_dir():
-                    yield from file_search(entry.path, *filter_options)
+            # 一致する場合のみyield
+            yield from match()
 
-    except PermissionError:
-        pass
+            # サブディレクトリがあれば再帰的に探索
+            if entry.is_dir(follow_symlinks=False):
+                yield from inner(entry.path, *filter_options)
+
+    # 最初のディレクトリから探索を開始
+    yield from inner(target_dir_path, *filter_options)
+
+    # 結果の時間を表示
+    scan_time.show_time()
+    match_time.show_time()
