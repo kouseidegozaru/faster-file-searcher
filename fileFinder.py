c51555c092fa144d2c5d834bf84df12509326b2e
kouseidegozaru <gguangchui@gmail.com>
2024-10-10 11:46:26 +0900

    検索条件が正しく適用されない問題を修正 4.0s

--- fileFinder.py
+++ fileFinder.py
@@ -1,10 +1,8 @@
 import os
 
-def is_match(file_name, filter_options):
-    # filter_optionsをセットに変換
-    filter_set = set(filter_options)
+def is_match(file_name, *filter_options):
     # 全てのフィルター条件を満たすか確認
-    return filter_set.issubset(file_name)
+    return all(filter_option in file_name for filter_option in filter_options)
 
 def file_search(target_dir_path, *filter_options):
     try:
@@ -13,7 +11,7 @@ def file_search(target_dir_path, *filter_options):
             for entry in entries:
 
                 # フィルターを適用する
-                if is_match(entry.name, filter_options):
+                if is_match(entry.name, *filter_options):
                     yield entry.path  # ファイルパスを返す
 
                 # サブディレクトリを探索する
